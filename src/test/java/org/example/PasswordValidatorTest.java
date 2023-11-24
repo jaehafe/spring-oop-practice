7733039a7f40c4a6b4b3fd888bb0b580eac4351a
Jaeha Lee <wogkdkrm112@gmail.com>
2023-11-25 01:40:52 +0900

    init: init oop practice project

---
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class PasswordValidatorTest {
+
+    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
+    @Test
+    void validatePasswordTest() {
+        assertThatCode(() -> PasswordValidator.validate("serveradam"))
+                .doesNotThrowAnyException();
+    }
+}
