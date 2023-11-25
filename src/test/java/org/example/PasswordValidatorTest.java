2196b2e48601aff24418b230cbbc82a3184496e5
Jaeha Lee <wogkdkrm112@gmail.com>
2023-11-25 12:27:26 +0900

    feat: ParameterizedTest

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -2,9 +2,16 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+/**
+ * 비밀번호는 최소 8자 이상 12자 이어여야 한다.
+ * 비밀번호가 8자 미만 또는 12자 초과인 경우 IleegalArgumentException 예외를 발생시킨다.
+ * 경계조건에 대해 테스트 코드를 작성해야 한다.
+ */
 public class PasswordValidatorTest {
 
     @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
@@ -12,5 +19,15 @@ public class PasswordValidatorTest {
     void validatePasswordTest() {
         assertThatCode(() -> PasswordValidator.validate("serveradam"))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 에러가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"aabbcce", "aabbccddeeffg"})
+    void validatePasswordTest2(String password) {
+
+        assertThatCode(() -> PasswordValidator.validate("aaaa"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
     }
 }
