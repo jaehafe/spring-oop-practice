5630b9fa397cc96d4971c14cc7ea0da5eab54bc5
jaehafe <wogkdkrm112@gmail.com>
2023-11-29 08:13:02 +0900

    feat: subtraction test

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Calculator {
     public static int calculate(int operand1, String operator, int operand2) {
-        return operand1 + operand2;
+        if ("+".equals(operator)) {
+            return operand1 + operand2;
+        } else if ("-".equals(operator)) {
+            return operand1 - operand2;
+        }
+        return 0;
     }
 }
