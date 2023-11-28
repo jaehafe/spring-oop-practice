29e257427b8837c93d7dbd8688090064825fb3c4
jaehafe <wogkdkrm112@gmail.com>
2023-11-29 08:29:20 +0900

    feat: Parameterized test

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
             return operand1 + operand2;
         } else if ("-".equals(operator)) {
             return operand1 - operand2;
+        } else if ("*".equals(operator)) {
+            return operand1 * operand2;
+        } else if ("/".equals(operator)) {
+            return operand1 / operand2;
         }
         return 0;
     }
