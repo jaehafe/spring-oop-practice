3b15fb55f479c1a31c4ce50d61c4cf7d5120d0d9
Jaeha Lee <wogkdkrm112@gmail.com>
2023-11-28 23:15:00 +0900

    feat: Calculator Test 코드 작성

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    // 1 + 2 -> cal
+    // 3 <-
+    @DisplayName("덧셈 연산을 수행한다.")
+    @Test
+    void additionTest() {
+        int result = Calculator.calculate(1, "+", 2);
+
+        assertThat(result).isEqualTo(3);
+    }
+}
